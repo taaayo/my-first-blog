8f379f23ea4feb1275953d4ddae463119cf99c44
tayo.try <tayo.try@gmail.com>
2021-07-05 15:23:26 +0900

    My Django Girls app, first commit

---
+++ myvenv/Scripts/deactivate.bat
@@ -0,0 +1,23 @@
+@echo off
+
+set PROMPT=$p$g
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+)
+set _OLD_VIRTUAL_PROMPT=
+
+if defined _OLD_VIRTUAL_PYTHONHOME (
+    set "PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%"
+    set _OLD_VIRTUAL_PYTHONHOME=
+)
+
+if defined _OLD_VIRTUAL_PATH (
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+)
+
+set _OLD_VIRTUAL_PATH=
+
+set VIRTUAL_ENV=
+
+:END
